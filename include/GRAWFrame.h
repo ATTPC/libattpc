a80d493ee4ed9773cbe0fb2adb542c7e5415e2b9
Joshua Bradt <bradt@nscl.msu.edu>
2017-09-12 11:47:59 -0400

    Added GRAWHeaderFields with dynamic constants.

--- include/GRAWFrame.h
+++ include/GRAWFrame.h
@@ -9,34 +9,59 @@
 namespace attpc {
 namespace mergers {
 
+template <class T>
+struct GRAWHeaderField {
+    using type = T;
+
+    explicit GRAWHeaderField(const size_t offset_)
+    : offset(offset_)
+    , size(1)
+    , value(0)
+    {}
+    GRAWHeaderField(const size_t offset_, const size_t size_)
+    : offset(offset_)
+    , size(size_)
+    , value(0)
+    {}
+    GRAWHeaderField(const size_t offset_, const size_t size_, const type value_)
+    : offset(offset_)
+    , size(size_)
+    , value(value_)
+    {}
+
+    size_t offset;
+    size_t size;
+    type value;
+};
+
 struct GRAWHeader {
-    uint8_t metaType;
-    uint32_t frameSize;
-    uint8_t dataSource;
-    uint16_t frameType;
-    uint8_t revision;
-    uint16_t headerSize;
-    uint16_t itemSize;
-    uint32_t itemCount;
-    uint64_t eventTime;
-    uint32_t eventIdx;
-    uint8_t coboIdx;
-    uint8_t asadIdx;
-    uint16_t readOffset;
-    uint8_t status;
-    std::bitset<9*8> hitPat_0;
-    std::bitset<9*8> hitPat_1;
-    std::bitset<9*8> hitPat_2;
-    std::bitset<9*8> hitPat_3;
-    uint16_t multip_0;
-    uint16_t multip_1;
-    uint16_t multip_2;
-    uint16_t multip_3;
-    uint32_t windowOut;
-    uint16_t lastCell_0;
-    uint16_t lastCell_1;
-    uint16_t lastCell_2;
-    uint16_t lastCell_3;
+    GRAWHeaderField<uint8_t> metaType {0};
+    GRAWHeaderField<uint32_t> frameSize {1, 3};
+    GRAWHeaderField<uint8_t> dataSource {4};
+    GRAWHeaderField<uint16_t> frameType {5, 2};
+    GRAWHeaderField<uint8_t> revision {7};
+    GRAWHeaderField<uint16_t> headerSize {8, 2};
+    GRAWHeaderField<uint16_t> itemSize {10, 2};
+    GRAWHeaderField<uint32_t> itemCount {12, 4};
+    GRAWHeaderField<uint64_t> eventTime {16, 6};
+    GRAWHeaderField<uint32_t> eventIdx {22, 4};
+    GRAWHeaderField<uint8_t> coboIdx {26};
+    GRAWHeaderField<uint8_t> asadIdx {27};
+    GRAWHeaderField<uint16_t> readOffset {28, 2};
+    GRAWHeaderField<uint8_t> status {30};
+    GRAWHeaderField<std::bitset<9*8>> hitPat_0 {31, 9};
+    GRAWHeaderField<std::bitset<9*8>> hitPat_1 {40, 9};
+    GRAWHeaderField<std::bitset<9*8>> hitPat_2 {49, 9};
+    GRAWHeaderField<std::bitset<9*8>> hitPat_3 {58, 9};
+    GRAWHeaderField<uint16_t> multip_0 {67, 2};
+    GRAWHeaderField<uint16_t> multip_1 {69, 2};
+    GRAWHeaderField<uint16_t> multip_2 {71, 2};
+    GRAWHeaderField<uint16_t> multip_3 {73, 2};
+    GRAWHeaderField<uint32_t> windowOut {75, 4};
+    GRAWHeaderField<uint16_t> lastCell_0 {79, 2};
+    GRAWHeaderField<uint16_t> lastCell_1 {81, 2};
+    GRAWHeaderField<uint16_t> lastCell_2 {83, 2};
+    GRAWHeaderField<uint16_t> lastCell_3 {85, 2};
 };
 
 struct GRAWDataItem {
@@ -59,7 +84,7 @@ struct GRAWFrame {
     GRAWHeader header;
     std::vector<GRAWDataItem> data;
 
-    bool isFullReadout() const { return header.frameType == SpecConstants::fullReadoutFrameType; }
+    bool isFullReadout() const { return header.frameType.value == SpecConstants::fullReadoutFrameType; }
 };
 
 }
