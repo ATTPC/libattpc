d065b5a3f12dc50acfae0a20affcf6595710cceb
Joshua Bradt <bradt@nscl.msu.edu>
2017-09-14 11:30:54 -0400

    Tested byte-swapping

--- include/utilities.h
+++ include/utilities.h
@@ -15,13 +15,16 @@ T parseValue(const Iterator& begin, const Iterator& end) {
     static_assert(!(std::is_integral<T>::value && std::is_signed<T>::value),
                   "Left bit shifts on signed integers can invoke undefined behavior, so this is not allowed.");
     assert(end > begin);
-    assert(static_cast<size_t>(std::distance(begin, end)) <= sizeof(T));
+
+    const size_t numBytes = static_cast<size_t>(std::distance(begin, end));
+    assert(numBytes <= sizeof(T));
 
     T result = 0;
+    unsigned offset = 0;
     for (auto iter = end - 1; iter >= begin; --iter) {
-        const unsigned offset = static_cast<unsigned>(std::distance(begin, iter));
         T byte = *iter;
         result |= (byte << offset*8u);
+        ++offset;
     }
     return result;
 }
