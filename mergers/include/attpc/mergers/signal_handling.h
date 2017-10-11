39a771f660ce52e9a21b9a730d0ab00d7bb1026b
Joshua Bradt <bradt@nscl.msu.edu>
2017-10-11 17:17:33 -0400

    Documented signal handler.

--- mergers/include/attpc/mergers/signal_handling.h
+++ mergers/include/attpc/mergers/signal_handling.h
@@ -1,4 +1,27 @@
 #include <csignal>
+/**
+ * @file
+ * @brief A basic signal handler for the merger.
+ *
+ * This implements a very basic signal handler that can catch a signal like SIGINT (usually issued by
+ * control-C). To use it, assign ::abortHandler as a signal handler using std::signal, and then
+ * later check whether the signal was received using ::abortWasCalled.
+ *
+ * If you improve this signal handler later, note that signal handlers can only perform an extremely
+ * restricted set of operations according to the standard, and doing much more could be undefined behavior.
+ */
 
+/**
+ * @brief A signal handler that sets a flag when it is called.
+ *
+ * Check the status of this flag with ::abortWasCalled.
+ *
+ * @param signal The signal number. This parameter is unused, but required by the standard.
+ */
 extern "C" void abortHandler(int signal);
+
+/**
+ * @brief Check on the status of the signal flag.
+ * @return True if the signal was received and the program should abort.
+ */
 bool abortWasCalled();
--- mergers/src/signal_handling.cpp
+++ mergers/src/signal_handling.cpp
@@ -1,5 +1,9 @@
 #include "attpc/mergers/signal_handling.h"
 
+/* This flag is set to 1 when the signal is received.
+ * About the only thing that a signal handler can do is modify a static volatile variable
+ * of this type, so that's what we do.
+ */
 static volatile std::sig_atomic_t abortFlag = 0;
 
 extern "C" void abortHandler(int) {
