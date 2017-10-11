61a0550377e9d02136da9bd0812cdd86dd344225
Joshua Bradt <bradt@nscl.msu.edu>
2017-10-11 17:07:25 -0400

    Documented RawFrame.

--- mergers/include/attpc/mergers/RawFrame.h
+++ mergers/include/attpc/mergers/RawFrame.h
@@ -7,25 +7,45 @@
 namespace attpc {
 namespace mergers {
 
+/**
+ * @brief Container for raw frame data read from a GRAWFile.
+ *
+ * This holds a fixed-size block of memory and provides iterators into it. That is all.
+ */
 class RawFrame {
 public:
+    //! The type of the contained data
     using byte_type = uint8_t;
+    //! An iterator into the data
     using iterator = byte_type*;
+    //! A constant iterator into the data
     using const_iterator = const byte_type*;
 
+    /**
+     * @brief Construct a new RawFrame buffer
+     * @param  size The number of bytes to allocate
+     */
     explicit RawFrame(const size_t size) : m_data(new byte_type[size]), m_size(size) {}
 
+    //! Get an iterator to the beginning of the data
     iterator begin() { return begin_impl(); }
+    //! @overload
     iterator begin() const { return begin_impl(); }
 
+    //! Get an iterator to the end of the data
     iterator end() { return end_impl(); }
+    //! @overload
     iterator end() const { return end_impl(); }
 
+    //! Get a constant iterator to the beginning of the data
     const_iterator cbegin() const { return begin_impl(); }
+    //! Get a constant iterator to the end of the data
     const_iterator cend() const { return end_impl(); }
 
+    //! Returns the size of the data, in bytes
     size_t size() const { return m_size; }
 
+    //! Gets a pointer to the raw data
     byte_type* getDataPtr() const { return m_data.get(); }
 
 private:
