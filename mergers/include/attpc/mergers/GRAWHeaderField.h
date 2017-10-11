16fa38418d8523363a8af1e138df217738754546
Joshua Bradt <bradt@nscl.msu.edu>
2017-10-11 15:50:44 -0400

    Documented GRAWHeaderField

--- mergers/include/attpc/mergers/GRAWHeaderField.h
+++ mergers/include/attpc/mergers/GRAWHeaderField.h
@@ -8,18 +8,59 @@
 namespace attpc {
 namespace mergers {
 
+/**
+ * @brief A single field in the GRAWHeader, including type, offset, and size information.
+ *
+ * This class provides a way of encoding the offset, size, and type of each field in the
+ * GRAWHeader directly in the field itself. Therefore, this information only has to be written
+ * in one place in the library (in the GRAWHeader header file), making it easier to maintain.
+ * In the future, this could also be read from a configuration file, though that would
+ * prevent the offset and size from being known at compile time.
+ *
+ * Aside from the increased complexity, the one disadvantage of this approach is that you have
+ * to access the value of the field through the member GRAWHeaderField::value instead of
+ * using it directly.
+ *
+ * @tparam T       The type used to represent the value of the field
+ * @tparam offset_ The offset from the beginning of the raw header to the encoded value
+ * @tparam size_   The number of bytes that the field occupies in the raw header
+ */
 template <class T, size_t offset_, size_t size_ = 1>
 struct GRAWHeaderField {
     static_assert(sizeof(T) >= size_, "Declared type is too small to hold a value with the given size");
 
+    //! Construct an empty field
     GRAWHeaderField() = default;
+    /**
+     * @brief Construct a field using the value from the raw header
+     *
+     * The header is parsed using extractFromRawFrame. The value will be read using the offset
+     * and size specified in the template parameters.
+     *
+     * @param rawFrame The raw frame from the GRAW file
+     */
     GRAWHeaderField(const RawFrame& rawFrame) : value(extractFromRawFrame(rawFrame)) {}
 
+    //! The type used to represent the field's value
     using type = T;
+    //! The offset into the raw header where the value is encoded
     static constexpr size_t offset = offset_;
+    //! The size of the encoded value in the header, in bytes
     static constexpr size_t size = size_;
+    //! The value of the field in the header
     type value;
 
+    /**
+     * @brief Parse the raw frame to get the value of this field
+     *
+     * The value is read using the offset and size given as template parameters to the class.
+     * Byte-swapping is performed automatically.
+     *
+     * @param  rawFrame The raw frame from the GRAW file.
+     * @return          The value of this field in the header.
+     *
+     * @throws std::runtime_error If the end of the field (offset + size) is past the end of the raw frame
+     */
     static type extractFromRawFrame(const RawFrame& rawFrame) {
         const auto fieldBegin = rawFrame.begin() + offset;
         const auto fieldEnd = fieldBegin + size;
