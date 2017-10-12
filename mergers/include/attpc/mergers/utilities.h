8bad4a4ccd6c21ae52cb32d904e3f14aba85a7f4
Joshua Bradt <bradt@nscl.msu.edu>
2017-10-12 11:22:31 -0400

    Documented utilities

--- mergers/include/attpc/mergers/utilities.h
+++ mergers/include/attpc/mergers/utilities.h
@@ -1,6 +1,11 @@
 #ifndef ATTPC_MERGERS_UTILITIES_H
 #define ATTPC_MERGERS_UTILITIES_H
 
+/**
+ * @file
+ * @brief Utilities for the merging code.
+ */
+
 #include <cstddef>
 #include <type_traits>
 #include <iterator>
@@ -10,6 +15,21 @@ namespace attpc {
 namespace mergers {
 namespace utilities {
 
+/**
+ * @brief Construct and byte-swap a value from a collection of bytes.
+ *
+ * This takes a collection of bytes (from a std::vector<uint8_t>, for example) and puts them together into
+ * a byte-swapped integer. The returned value is of type `T`, where `T` must not be a signed integer since
+ * that would invoke undefined behavior (by using the left-shift operator).
+ *
+ * @tparam T        The type of value to construct as a result.
+ * @tparam Iterator The type of the begin and end iterators.
+ *
+ * @param  begin Iterator to the first byte.
+ * @param  end   Iterator to the element past the last byte.
+ *
+ * @return       The byte-swapped value.
+ */
 template <class T, class Iterator>
 T parseValue(const Iterator& begin, const Iterator& end) {
     static_assert(!(std::is_integral<T>::value && std::is_signed<T>::value),
